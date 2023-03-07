8eeb3ad67fc6c6781fc90aa001449af6db888f4d
hhuston <90809677+hhuston@users.noreply.github.com>
2023-03-07 12:20:37 -0500

    Project Deletion Functionality

--- backend/data/projects.js
+++ backend/data/projects.js
@@ -1,4 +1,5 @@
 import { projects } from "../config/mongoCollections.js";
+import { closeConnection, dbConnection } from "../config/mongoConnection.js";
 
 const getAllProjects = async () => {
   let projectsCollection;
@@ -10,5 +11,19 @@ const getAllProjects = async () => {
   const res = await projectsCollection.find({}).toArray();
   return res;
 };
+
+const deleteProject = async (projectID) => {
+  const database = await dbConnection();
+  let projectsCollection = await database.collection("projects");
+
+  const deleteResult = projectsCollection.deleteOne({_id : projectID});
+
+  if (deleteResult.deletedCount == 1) {
+    console.log("Successfully deleted one project with ID ${projectID}");
+  }
+  else {
+    console.log("Could not delete project with ID ${projectID}")
+  }
+}
 
 export { getAllProjects };
