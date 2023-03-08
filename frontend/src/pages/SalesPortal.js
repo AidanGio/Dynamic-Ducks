684e3c29e9ea709022a81ed66ea68c8e9e373adc
invissy <72313458+invissy@users.noreply.github.com>
2023-03-07 22:03:37 -0500

    more styling

--- frontend/src/pages/SalesPortal.js
+++ frontend/src/pages/SalesPortal.js
@@ -61,7 +61,7 @@ const SalesPortal = () => {
   return (
   <SalesLayout>
     <h1>Sales Portal</h1>
-    <div>
+    <div style={{boxShadow: "0px 0px 0px 1px rgb(0, 0, 0)", padding: "0px 50px"}}>
       <h2>Current Projects</h2>
         <ProjectTable projects={dummyProjects} />
       <h2>Past Projects</h2>
