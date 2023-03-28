bdfd9619c05ca32b019a5ee8c8b798aa039cb271
Arjun Khatri <akhatri1@stevens.edu>
2023-03-28 19:22:45 -0400

    Update Message.JS
    added dummy data

--- frontend/src/pages/Messages.js
+++ frontend/src/pages/Messages.js
@@ -27,7 +27,74 @@ const Messages = [
       "It's going well! We're on track to finish on time. How about your team?",
     timeSent: "2022-04-05T14:40:00",
   },
+  {
+    sender: "John",
+    recipient: "Sarah",
+    message: "Hey, how's it going?",
+    timeSent: "2023-03-28T10:15:00",
+  },
+  {
+    sender: "Sarah",
+    recipient: "John",
+    message: "Not bad, thanks. How about you?",
+    timeSent: "2023-03-28T10:17:00",
+  },
+  {
+    sender: "John",
+    recipient: "Sarah",
+    message: "I'm good, thanks. Just wanted to check in on you.",
+    timeSent: "2023-03-28T10:19:00",
+  },
+  {
+    sender: "Sarah",
+    recipient: "John",
+    message: "That's really nice of you. I'm doing okay, just busy with work.",
+    timeSent: "2023-03-28T10:21:00",
+  },
+  {
+    sender: "John",
+    recipient: "Sarah",
+    message: "I hear you. Let's catch up soon.",
+    timeSent: "2023-03-28T10:24:00",
+  },
+  {
+    sender: "Sarah",
+    recipient: "John",
+    message: "Definitely. Talk to you later.",
+    timeSent: "2023-03-28T10:26:00",
+  },
+  {
+    sender: "Michael",
+    recipient: "Olivia",
+    message: "Hi Olivia, can we meet to discuss the project tomorrow?",
+    timeSent: "2023-03-28T11:30:00",
+  },
+  {
+    sender: "Olivia",
+    recipient: "Michael",
+    message: "Sure, what time works for you?",
+    timeSent: "2023-03-28T11:33:00",
+  },
+  {
+    sender: "Michael",
+    recipient: "Olivia",
+    message: "How about 2pm?",
+    timeSent: "2023-03-28T11:35:00",
+  },
+  {
+    sender: "Olivia",
+    recipient: "Michael",
+    message: "Sounds good. Let's meet in the conference room.",
+    timeSent: "2023-03-28T11:37:00",
+  },
+  {
+    sender: "Michael",
+    recipient: "Olivia",
+    message: "Okay, see you tomorrow.",
+    timeSent: "2023-03-28T11:40:00",}
 ];
+
+
 
 function MessageRow({ message }) {
   return (
@@ -67,7 +134,7 @@ const MessagePortal = () => {
     <OperationsManagerLayout>
       <h1>Message Thread</h1>
       <div>
-       
+        
         <MessageTable messages={Messages} />
       </div>
     </OperationsManagerLayout>
