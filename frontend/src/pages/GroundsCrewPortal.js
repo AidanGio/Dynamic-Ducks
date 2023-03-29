0c146bd23ea9d79485734ebb43014cfad3b790da
ayushigup4 <125705383+ayushigup4@users.noreply.github.com>
2023-03-28 23:06:17 -0400

    Basic GroundsCrewPage layout

--- frontend/src/pages/GroundsCrewPortal.js
+++ frontend/src/pages/GroundsCrewPortal.js
@@ -2,68 +2,65 @@ import React from "react";
 import GroundCrewLayout from "../layouts/GroundCrewLayout";
 import "./styles.scss";
 
-const GroundsCrewPortal = () => {
-  const projects = [
-    {
-      id: 1,
-      name: "Project A",
-      startDate: "2022-01-01",
-      endDate: "2022-06-30",
-      stage: "Planning",
-      progress: 25,
-      laborHours: 100,
-      equipmentDetails: "Crane",
-      inspectionDetails: "TBA",
-      closeoutInfo: "TBA",
-    },
-    {
-      id: 2,
-      name: "Project B",
-      startDate: "2022-03-01",
-      endDate: "2022-08-31",
-      stage: "In Progress",
-      progress: 50,
-      laborHours: 200,
-      equipmentDetails: "Bucket Truck",
-      inspectionDetails: "Inspection passed",
-      closeoutInfo: "TBA",
-    },
-  ];
-  
+
+
+const dummyProjects = [
+  {name: "My Project 1", startDate: "2/15/2023", endDate: "4/14/2023", progress: "Validation", status: 85},
+  {name: "My Project 2", startDate: "2/15/2023", endDate: "4/14/2023", progress: "Installation", status: 60},
+];
+
+function ProjectRow({project}){
   return (
-    <GroundCrewLayout>
-      <h1>Grounds Crew Portal</h1>
-      <table className="my-table">
-        <thead>
-          <tr>
-            <th>Project Name</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>Stage</th>
-            <th>Progress %</th>
-            <th>Labor Hours</th>
-            <th>Equipment/Material Details</th>
-            <th>Inspection Details</th>
-            <th>Closeout Info</th>
-          </tr>
-        </thead>
-        <tbody>
-          {projects.map((project) => (
-            <tr key={project.id}>
-              <td>{project.name}</td>
-              <td>{project.startDate}</td>
-              <td>{project.endDate}</td>
-              <td>{project.stage}</td>
-              <td>{project.progress}%</td>
-              <td>{project.laborHours}</td>
-              <td>{project.equipmentDetails}</td>
-              <td>{project.inspectionDetails}</td>
-              <td>{project.closeoutInfo}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </GroundCrewLayout>
+    <tr>
+      <td>{project.name}</td>
+      <td>{project.startDate}</td>
+      <td>{project.endDate}</td>
+      <td>{project.progress}</td>
+      <td>{project.status}</td>
+    </tr>
+  );
+};
+function ProjectTable({projects, active}){
+  const rows = [];
+
+  
+  // Need to filter projects so that the table will either only have current projects or past projects
+  // Tried doing it this way but it makes it so that the entire page disappears...
+  projects.forEach((project) => {
+    //if ((active && project.endDate.stringToDate().getTime() < new Date.getTime()) || (!active && project.endDate.stringToDate().getTime() > new Date.getTime())){
+    rows.push(
+      <ProjectRow project = {project} />
+    );
+    //}
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Start Date</th>
+          <th>End Date</th>
+          <th>Progress</th>
+          <th>Project Status</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
+
+const GroundCrewPortal = () => {
+  return (
+  <GroundCrewLayout>
+    <h1>Ground Crew Portal</h1>
+    <div>
+      <h2>Current Projects</h2>
+        <ProjectTable projects={dummyProjects} />
+      <h2>Past Projects</h2>
+        <ProjectTable projects={dummyProjects}  />
+    </div>
+  </GroundCrewLayout>
   );
 };
 
