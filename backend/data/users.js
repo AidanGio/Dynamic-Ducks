78b9265f53e5fc3eb8882e9968547e660d57c434
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2023-04-03 17:09:51 -0400

    User Login/Register - 50%

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,6 +1,8 @@
 import { ObjectId } from "mongodb";
 import { users } from "../config/mongoCollections.js";
 
+import bcrypt from "bcrypt";
+
 const createUser = async (obj) => {
   const usersCollection = await users();
 
@@ -9,13 +11,34 @@ const createUser = async (obj) => {
   if (!insertInfo.acknowledged || !insertInfo.insertedId)
     throw { status: 400, msg: "Could not add users" };
 
-  return insertInfo;
+  return await getUserById(insertInfo.insertedId);
+};
+
+const userLogin = async ({ email, password }) => {
+  const usersCollection = await users();
+
+  const findUser = await usersCollection.findOne({ email: email });
+
+  if (findUser == null) {
+    throw { status: 404, message: "User not found" };
+  }
+  const dbPassword = findUser.password;
+
+  const validPassword = await bcrypt.compare(password, dbPassword);
+
+  if (validPassword) {
+    delete findUser.password;
+    return { ...findUser };
+  } else throw { status: 401, message: "Error: Invalid Username or Password" };
 };
 
 const getUserById = async (id) => {
   const usersCollection = await users();
 
-  const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+  const user = await usersCollection.findOne(
+    { _id: new ObjectId(id) },
+    { projection: { password: 0 } }
+  );
 
   return user;
 };
@@ -33,4 +56,4 @@ const getAllUsers = async () => {
   return usersData;
 };
 
-export { createUser, getUserById, deleteUser, getAllUsers };
+export { createUser, getUserById, deleteUser, getAllUsers, userLogin };
--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -1,36 +1,49 @@
 import React, { useState } from "react";
 import MainLayout from "../layouts/MainLayout";
+import { apiInstance } from "../utils/apiInstance";
 
 const SignInPage = () => {
-  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const [role, setRole] = useState("Client Portal");
+  const [error, setError] = useState();
+  // const [role, setRole] = useState("Client Portal");
 
-  const submit = (e) => {
+  const submit = async (e) => {
     e.preventDefault();
-    console.log(username);
+    console.log(email);
     console.log(password);
-    console.log(role);
 
-    switch (role) {
-      case "Client Portal":
-        window.location.href = "/clientportal";
-        break;
-      case "Sales Portal":
-        window.location.href = "/salesportal";
-        break;
-      case "Grounds Crew Portal":
-        window.location.href = "/groundscrewportal";
-        break;
-      case "Operations Manager Portal":
-        window.location.href = "/operationsmanagerportal";
-        break;
-      case "Messages":
-        window.location.href = "/messages";
-        break;
-      default:
-        console.log("Invalid role selected.");
+    try {
+      const data = await apiInstance.post("/users/login",  {
+        email,
+        password,
+        
+      });
+
+      console.log(data);
+    } catch (error) {
+      setError({ error: true, message: error.message });
     }
+
+    // switch (role) {
+    //   case "Client Portal":
+    //     window.location.href = "/clientportal";
+    //     break;
+    //   case "Sales Portal":
+    //     window.location.href = "/salesportal";
+    //     break;
+    //   case "Grounds Crew Portal":
+    //     window.location.href = "/groundscrewportal";
+    //     break;
+    //   case "Operations Manager Portal":
+    //     window.location.href = "/operationsmanagerportal";
+    //     break;
+    //   case "Messages":
+    //     window.location.href = "/messages";
+    //     break;
+    //   default:
+    //     console.log("Invalid role selected.");
+    // }
   };
 
   return (
@@ -38,16 +51,16 @@ const SignInPage = () => {
       <h1>SIGN IN</h1>
       <form onSubmit={(e) => submit(e)}>
         <input
-          placeholder="Username"
-          type={"text"}
-          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Email"
+          type={"email"}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           placeholder="Password"
           type={"password"}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <select
+        {/* <select
           className="role-dropdown"
           value={role}
           onChange={(e) => setRole(e.target.value)}
@@ -59,7 +72,7 @@ const SignInPage = () => {
             Operations Manager Portal
           </option>
           <option value="Messages">Messages</option>
-        </select>
+        </select> */}
         <button>Submit</button>
       </form>
     </MainLayout>
