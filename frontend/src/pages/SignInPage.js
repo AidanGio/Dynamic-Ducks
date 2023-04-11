8203cb5f1492b92cfa1d7f6099f711f090e8d30e
Ubuntu <hhuston@stevens.edu>
2023-04-11 14:24:07 -0400

    Sign In Page

--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 import { apiInstance } from "../utils/apiInstance";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/solar.png";
 
 const SignInPage = () => {
   const [email, setEmail] = useState();
@@ -12,7 +14,6 @@ const SignInPage = () => {
     e.preventDefault();
     console.log(email);
     console.log(password);
-
     try {
       const data = await apiInstance.post("/users/login",  {
         email,
@@ -48,33 +49,47 @@ const SignInPage = () => {
 
   return (
     <MainLayout>
-      <h1>SIGN IN</h1>
-      <form onSubmit={(e) => submit(e)}>
-        <input
-          placeholder="Email"
-          type={"email"}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          placeholder="Password"
-          type={"password"}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {/* <select
-          className="role-dropdown"
-          value={role}
-          onChange={(e) => setRole(e.target.value)}
-        >
-          <option value="Client Portal">Client Portal</option>
-          <option value="Sales Portal">Sales Portal</option>
-          <option value="Grounds Crew Portal">Grounds Crew Portal</option>
-          <option value="Operations Manager Portal">
-            Operations Manager Portal
-          </option>
-          <option value="Messages">Messages</option>
-        </select> */}
-        <button>Submit</button>
-      </form>
+      <div style={{  width:"100vw" , height:"90vh" , padding:"0", margin:"0", display:"flex" , alignItems:"center" , justifyContent:"center"}}>
+        <div style={{ width:"50%" , height:"100%" , background:"linear-gradient(to bottom right, #f4afab, #f4eea9)" }}>
+          <image src={logo} height={"100%"}/>
+        </div>
+        <div style={{ width:"50%" , height:"100%" , paddingTop:"50vh" }}>
+          <h1>SIGN IN</h1>
+          <form style={{ paddingLeft:"17.5vw" }} onSubmit={(e) => submit(e)}>
+          <h4 style={{ textAlign:"left" , paddingLeft:"10px" }}>Email</h4>
+          <input
+            placeholder="Email Address"
+            type={"email"}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h4 style={{ textAlign:"left" , paddingLeft:"10px" }}>Password</h4>
+          <input
+            placeholder="Password"
+            type={"password"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Link to={"./ForgotPassword.js"} style={{ textDecoration:"none" , textAlign:"right" , 
+              paddingRight:"10px" , color:"black"}}>
+            Forgot Password?
+          </Link>
+            {/* <select
+              className="role-dropdown"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <option value="Client Portal">Client Portal</option>
+              <option value="Sales Portal">Sales Portal</option>
+              <option value="Grounds Crew Portal">Grounds Crew Portal</option>
+              <option value="Operations Manager Portal">
+                Operations Manager Portal
+              </option>
+              <option value="Messages">Messages</option>
+            </select> */}
+          <button style={{ backgroundImage:"linear-gradient(to right, #fdbf61, #f4eea9)" ,   
+          color:"black", border:"1px solid black" , }}>Sign In</button>
+          </form>
+        </div>
+      </div>
     </MainLayout>
   );
 };
