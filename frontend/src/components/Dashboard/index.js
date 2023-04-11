153f38836d0a54a7f2d2e0c40c7abfec2e5bad69
AidanGio <agiordan@stevens.edu>
2023-04-11 18:52:37 -0400

    calendar view

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.scss";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 const Dashboard = () => {
+  const [calendarEvents, setCalendarEvents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5001/calendar/6407f77988b55420b2f3476c")
+      .then((response) => response.json())
+      .then((data) => {
+        const events = data.map((event) => ({
+          title: event.name,
+          start: new Date(event.startDate),
+          end: new Date(event.endDate),
+        }));
+        setCalendarEvents(events);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="dashboard">
-      <h2>Dashboard</h2>
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" events={calendarEvents} />
     </div>
   );
 };
+
+
+
 export default Dashboard;
+
