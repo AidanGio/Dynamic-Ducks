93282ed2edf1500d9629c0ec288a6a72e0b1b53d
invissy <72313458+invissy@users.noreply.github.com>
2023-04-11 20:43:07 -0400

    added stuff to EditLeadPage for demo

--- frontend/src/pages/EditLeadPage.js
+++ frontend/src/pages/EditLeadPage.js
@@ -1,13 +1,49 @@
 import React, { useEffect, useState } from "react";
 import { apiInstance } from "../utils/apiInstance";
 import {useLocation} from 'react-router-dom';
+import SalesLayout from "../layouts/SalesLayout";
 
 const EditLeadPage = () => {
 // if lead exists, update lead
 // if not, create new lead
 
+return (
+    // Hardcoded for demo purposes
+    <SalesLayout>
+        <h1>Edit Lead</h1>
 
+        <div>
+            First Name
+            <input type="text" placeholder="" />
+        </div>
+        
+        <div>
+            Last Name
+            <input type="text" placeholder="" />
+        </div>
 
+        <div>
+            Phone Number
+            <input type="text" placeholder="" />
+            ext
+            <input type="text" placeholder="" />
+        </div>
+
+        <div>
+            Email
+            <input type="text" placeholder="" />
+        </div>
+
+        <div>
+            Status
+            <input type="checkbox" /> Following Up
+            <input type="checkbox" /> Success
+        </div>
+
+        <button>Update Lead</button>
+        
+    </SalesLayout>
+);
     
 }
 
