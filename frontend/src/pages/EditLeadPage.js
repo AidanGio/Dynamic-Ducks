8e1f866cacf8cc5f8052528545cf05889aee9c5c
invissy <72313458+invissy@users.noreply.github.com>
2023-04-22 19:43:46 -0400

    started to implement EditLeadPage

--- frontend/src/pages/EditLeadPage.js
+++ frontend/src/pages/EditLeadPage.js
@@ -1,50 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { apiInstance } from "../utils/apiInstance";
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import SalesLayout from "../layouts/SalesLayout";
 
 const EditLeadPage = () => {
-// if lead exists, update lead
-// if not, create new lead
+    // if lead exists, update lead
+    // if not, create new lead
 
-return (
-    // Hardcoded for demo purposes
-    <SalesLayout>
-        <h1>Edit Lead</h1>
+    const location = useLocation();
+    const lead = location.state;
+    const leadExists = lead !== undefined;
 
-        <div>
-            First Name
-            <input type="text" placeholder="" />
-        </div>
-        
-        <div>
-            Last Name
-            <input type="text" placeholder="" />
-        </div>
+    function submitButton() {
 
-        <div>
-            Phone Number
-            <input type="text" placeholder="" />
-            ext
-            <input type="text" placeholder="" />
-        </div>
+    }
 
-        <div>
-            Email
-            <input type="text" placeholder="" />
-        </div>
+    return (
+        // Hardcoded for demo purposes
+        <SalesLayout>
+            <h1>{leadExists ? "Edit Lead" : "Create Lead"}</h1>
 
-        <div>
-            Status
-            <input type="checkbox" /> Following Up
-            <input type="checkbox" /> Success
-        </div>
+            <div>
+                First Name
+                <input type="text" placeholder="" />
+            </div>
 
-        <button>Update Lead</button>
-        
-    </SalesLayout>
-);
-    
+            <div>
+                Last Name
+                <input type="text" placeholder="" />
+            </div>
+
+            <div>
+                Phone Number
+                <input type="text" placeholder="" />
+                ext
+                <input type="text" placeholder="" />
+            </div>
+
+            <div>
+                Email
+                <input type="text" placeholder="" />
+            </div>
+
+            <div>
+                Status
+                <input type="checkbox" /> Following Up
+                <input type="checkbox" /> Success
+            </div>
+
+            <button>Update Lead</button>
+
+        </SalesLayout>
+    );
+
 }
 
 export default EditLeadPage;
