b9365d4db42d68b35505ff4b5b181f5ca6063127
Egnifluro <126028000+Egnifluro@users.noreply.github.com>
2023-05-04 18:05:23 -0400

    Update ClientPortal.js

--- frontend/src/pages/ClientPortal.js
+++ frontend/src/pages/ClientPortal.js
@@ -1,8 +1,18 @@
 import React from "react";
 import MainLayout from "../layouts/MainLayout";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const ClientPortal = ({ auth }) => {
-  return <MainLayout auth={auth}>ClientPortal</MainLayout>;
+  let navigate = useNavigate();
+  return <MainLayout auth={auth}>ClientPortal
+    <Button variant={"contained"} onClick={() => navigate(`/users/upload/${auth._id}`)}>
+          Photo Upload
+  </Button>
+  <Button variant={"contained"} onClick={() => navigate(`/users/view/${auth._id}`)}>
+          All Photos
+  </Button>
+  </MainLayout>;
 };
 
 export default ClientPortal;
